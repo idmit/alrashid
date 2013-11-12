8975a8a592095e4f54f5a51f13df7119e4c7f1dd
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2013-11-12 23:24:56 +0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  alrashid
+//
+//  Created by Ivan Dmitrievsky on 12.11.13.
+//  Copyright (c) 2013 Ivan Dmitrievsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
+
